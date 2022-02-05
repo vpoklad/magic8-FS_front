b9f24e45c948181b94858e0aebecd97ae1314049
Halyna-K <hkzlvs@gmail.com>
2022-02-05 19:25:38 +0200

    add store

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
 import App from './App';
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <PersistGate loading="Loading..." persistor={persistor}>
+      <Router>
+        <App />
+      </Router>
+    </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
