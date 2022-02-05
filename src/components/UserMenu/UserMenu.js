fb473e751853426a3b5ec65941eff7a536650a01
Olha Tsyhanenko <olya.tsyganenko@gmail.com>
2022-02-05 18:30:30 +0200

    up sprite

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -1,15 +1,15 @@
-import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 import useMediaQuery from '@mui/material/useMediaQuery';
+import sprite from '../../sprite.svg';
 import s from './UserMenu.module.css';
 
 export default function UserMenu() {
-  const tablet = useMediaQuery('(min-width:768px)');
+  const tablet = useMediaQuery('(min-width: 768px)');
 
   return (
     <div className={s.header__menu}>
-      <div className={s.header__avatar}>M</div>
+      <div className={s.header__avatar}>H</div>
       {tablet && <>
-        <span className={s.header__username}>OlyaTsyhanenko</span>
+        <span className={s.header__username}>HelenMarlen</span>
         <div className={s.header__divider}></div>
       </>}
       <button
@@ -17,7 +17,10 @@ export default function UserMenu() {
         type="button"
         // onClick={() => dispatch(authOperations.logOut())}
       >
-        {tablet ? 'Вихід' : < LogoutOutlinedIcon className={s.logout__icon} />}
+        {tablet
+          ? 'Вихід'
+          : <svg width="16" height="16"><use href={`${sprite}#icon-logout`}></use></svg>
+        }
       </button>
     </div>
   );
