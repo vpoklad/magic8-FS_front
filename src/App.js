383442aaa3541c3f8b56228342150396c690cb74
SvitlanaDmytriv <svetagawrylyuk26@gmail.com>
2022-02-06 17:38:31 +0200

    add section balance

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import './App.css';
 import AppBar from './components/AppBar/AppBar';
 import AuthForm from './components/AuthForm/AuthForm';
+import Balance from './components/Balance/Balance';
 
 function App() {
   return (
     <div className="App">
       <AppBar />
       <AuthForm />
+      <Balance showReport={false} />
     </div>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,53 @@
 import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-  } from "redux-persist";
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+
 import authReducer from './auth/slices';
+import balanceSlice from './balance/slice';
 
 const authPersistConfig = {
-          key: 'auth',
-          storage,
-          whitelist: ['token'],
-}
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 const authPersistReducer = persistReducer(authPersistConfig, authReducer);
 
-const middleware = [...getDefaultMiddleware ({
-         serializableCheck: {
-           ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-         },
-     })
-     ];
+const balancePersistConfig = {
+  key: 'value',
+  storage,
+  whitelist: ['value'],
+};
+const balancePersistReducer = persistReducer(
+  balancePersistConfig,
+  balanceSlice,
+);
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
 
 const store = configureStore({
-         reducer: {
-           auth: authPersistReducer,
-         },
-         middleware,
-      devTools: process.env.NODE_ENV === 'development',
-  })
+  reducer: {
+    auth: authPersistReducer,
+    balance: balancePersistReducer,
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
+});
 
-const persistor = persistStore (store);
+const persistor = persistStore(store);
 
-export {store, persistor};
+export { store, persistor };
