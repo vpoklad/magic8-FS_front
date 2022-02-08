86b4a16fd0c47a05752b4225d27ec878919f11bf
SvitlanaDmytriv <svetagawrylyuk26@gmail.com>
2022-02-08 13:41:29 +0200

    add tooltip

--- src/redux/auth/slices.js
+++ src/redux/auth/slices.js
@@ -1,45 +1,45 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { register, logIn, logoutThunk, fetchCurrentUser } from "./thunks";
+import { createSlice } from '@reduxjs/toolkit';
+import { register, logIn, logoutThunk, fetchCurrentUser } from './thunks';
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState:{
-        email: null,
+  name: 'auth',
+  initialState: {
+    email: null,
+    token: null,
+    isAuth: false,
+    isFetchingCurrentUser: false,
+  },
+  extraReducers: {
+    [register.fulfilled](state, action) {
+      state.user = action.payload.email;
+      state.token = action.payload.token;
+      state.isAuth = false;
+    },
+    [logIn.fulfilled](state, action) {
+      state.user = action.payload.email;
+      state.token = action.payload.token;
+      state.isAuth = true;
+    },
+    [logoutThunk.fulfilled](state, _) {
+      return {
+        ...state,
+        user: { name: null, email: null },
         token: null,
         isAuth: false,
-        isFetchingCurrentUser: false,
+      };
     },
-    extraReducers: {
-         [register.fulfilled](state, action) {
-            state.user = action.payload.email;
-            state.token = action.payload.token;
-            state.isAuth = false;
-        },
-         [logIn.fulfilled](state, action) {
-            state.user = action.payload.email;
-            state.token = action.payload.token;
-            state.isAuth = true;
-        },
-         [logoutThunk.fulfilled](state,_){
-            return {
-                ...state,
-                user: { name: null, email: null },
-                token: null,
-                isAuth: false,
-            }
-        },
-         [fetchCurrentUser.pending](state) {
-            state.isFetchingCurrentUser = true;
-        },
-         [fetchCurrentUser.fulfilled](state, action) {
-            state.user = action.payload.email;
-            state.isAuth = true;
-            state.isFetchingCurrentUser = false;
-        },
-         [fetchCurrentUser.rejected](state) {
-            state.isFetchingCurrentUser = false;
-        }
-    }
-})
+    [fetchCurrentUser.pending](state) {
+      state.isFetchingCurrentUser = true;
+    },
+    [fetchCurrentUser.fulfilled](state, action) {
+      state.user = action.payload.email;
+      state.isAuth = true;
+      state.isFetchingCurrentUser = false;
+    },
+    [fetchCurrentUser.rejected](state) {
+      state.isFetchingCurrentUser = false;
+    },
+  },
+});
 
 export default authSlice.reducer;
