18ec9614f8848229009fcfbf960994c355addac7
SvitlanaDmytriv <svetagawrylyuk26@gmail.com>
2022-02-11 15:22:08 +0200

    fix modal balance

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,6 +1,7 @@
 export const getUser = state => state.auth.email;
 export const getToken = state => state.auth.token;
-export const getIsFetchingCurrentUser = state => state.auth.isFetchingCurrentUser;
+export const getIsFetchingCurrentUser = state =>
+  state.auth.isFetchingCurrentUser;
 export const getVerify = state => state.auth.verify;
 export const getFormError = state => state.auth.formError;
 export const getVerifyMessage = state => state.auth.verifyMessage;
