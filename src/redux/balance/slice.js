3c41247eb8341e5a2a4e6a1cd3d6cb49cb7497d8
SvitlanaDmytriv <svetagawrylyuk26@gmail.com>
2022-02-13 13:59:04 +0200

    fix balance

--- src/redux/balance/slice.js
+++ src/redux/balance/slice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { addBalanceThunk } from './thunk';
+import { addBalanceThunk, getBalanceThunk } from './thunk';
 
 const initialStateBalance = {
   value: null,
@@ -11,6 +11,18 @@ const balanceSlice = createSlice({
   name: 'balance',
   initialState: initialStateBalance,
   extraReducers: {
+    [getBalanceThunk.pending]: (state, _) => {
+      state.isLoading = true;
+    },
+    [getBalanceThunk.fulfilled]: (state, action) => {
+      state.value = action.payload;
+      state.isLoading = false;
+      state.error = null;
+    },
+    [getBalanceThunk.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
     [addBalanceThunk.pending]: (state, _) => {
       state.isLoading = true;
     },
