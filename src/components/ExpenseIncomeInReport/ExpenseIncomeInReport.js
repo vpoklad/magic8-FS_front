c42b03c3c52ea0b23d704f215b7ec74a03cbe6dc
Mykola Stotskyi <stotskyi.m@gmail.com>
2022-02-16 18:30:37 +0200

    some fix

--- src/components/ExpenseIncomeInReport/ExpenseIncomeInReport.js
+++ src/components/ExpenseIncomeInReport/ExpenseIncomeInReport.js
@@ -7,46 +7,45 @@ import { useSelector } from 'react-redux';
 import { getReports } from '../../redux/reports/selectors';
 
 export default function ExpenseIncomeInReport() {
-    const [expense, setExpense] = useState(true);
-    const [income, setIncome] = useState(false);
-    const dataReports = useSelector(getReports);
+  const [expense, setExpense] = useState(true);
+  const [income, setIncome] = useState(false);
+  const dataReports = useSelector(getReports);
 
-    const clickChange = () => {
-        if (expense) {
-            setIncome(true);
-            setExpense(false);
-        }
-        if (income) {
-            setIncome(false);
-            setExpense(true);
-        }
-    };
+  const clickChange = () => {
+    if (expense) {
+      setIncome(true);
+      setExpense(false);
+    }
+    if (income) {
+      setIncome(false);
+      setExpense(true);
+    }
+  };
 
-    return (
-        <div className={s.containerExpenseIncome}>
-            <div className={s.wrapper}>
-                <button
-                    className={s.btnChevron}
-                    onClick={clickChange}>
-                    <svg className={s.item_svg} width="4" height="10"><use href={`${sprite}#icon-chevronLeft`}></use></svg>
-                </button>
-                <p className={s.title}>{expense ? 'Витрати' : 'Доходи'}</p>
-                <button
-                    className={s.btnChevron}
-                    onClick={clickChange}>
-                    <svg className={s.item_svg} width="4" height="10"><use href={`${sprite}#icon-chevronRight`}></use></svg>
-                </button>
-            </div>
-            {dataReports &&
-                <>
-                {expense ? <ExpenseInReport data={dataReports} /> : <IncomeInReport data={dataReports} />}
-            </>
-            }
-            </div>
-
-    )
-
-
-
+  return (
+    <div className={s.containerExpenseIncome}>
+      <div className={s.wrapper}>
+        <button className={s.btnChevron} onClick={clickChange}>
+          <svg className={s.item_svg} width="6" height="11">
+            <use href={`${sprite}#icon-chevronLeft`}></use>
+          </svg>
+        </button>
+        <p className={s.title}>{expense ? 'Витрати' : 'Доходи'}</p>
+        <button className={s.btnChevron} onClick={clickChange}>
+          <svg className={s.item_svg} width="6" height="11">
+            <use href={`${sprite}#icon-chevronRight`}></use>
+          </svg>
+        </button>
+      </div>
+      {dataReports && (
+        <>
+          {expense ? (
+            <ExpenseInReport data={dataReports} />
+          ) : (
+            <IncomeInReport data={dataReports} />
+          )}
+        </>
+      )}
+    </div>
+  );
 }
-
