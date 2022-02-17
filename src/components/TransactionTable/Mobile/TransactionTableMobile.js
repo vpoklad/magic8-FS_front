28608747d2a5d9bd293a4844a80ae973fdf9559e
Anidamp <79271758+Anidamp@users.noreply.github.com>
2022-02-17 13:13:24 +0200

    fix

--- src/components/TransactionTable/Mobile/TransactionTableMobile.js
+++ src/components/TransactionTable/Mobile/TransactionTableMobile.js
@@ -1,10 +1,22 @@
 import s from './TransactionMobileTable.module.css'
 import TransactionMobile from './TransactionMobile'
+import { useState } from 'react';
+import DatePickerComponent from '../../DatePicker/DatePicker';
 
 
 const TransactionMobileTable = ({ transactions }) => {
+const initialDate = new Date();
+  const [date, setDate] = useState(initialDate);
+  
 
+  const getDate = value => {
+    setDate(value);
+  };
   return (
+    <div>
+      <div className={s.datepieckerWrapper}>
+           <DatePickerComponent getDate={getDate} />
+        </div>
       <ul className={s.mobileList}>
         {!transactions
           ? 'Чекайте'
@@ -16,6 +28,7 @@ const TransactionMobileTable = ({ transactions }) => {
             />)
           )}
       </ul>
+    </div>
   )
 }
 
