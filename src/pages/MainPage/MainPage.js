3669e57ee75e50a579036aea35d90c4c63fc5bf9
Mykola Stotskyi <stotskyi.m@gmail.com>
2022-02-17 13:49:41 +0200

    done

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -3,17 +3,13 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 import Balance from '../../components/Balance/Balance';
 import CountingTable from '../../components/CountingTable/CountingTable';
 import Report from '../../components/Report/Report';
-import DatePicker from '../../components/DatePicker/DatePicker';
 
 export default function MainPage() {
-  const mobile = useMediaQuery('(max-width: 767px)');
-
   return (
     <div className={s.container}>
       <div className={s.containerNav}>
         <Report />
         <Balance />
-        {mobile && <DatePicker />}
       </div>
       <CountingTable />
     </div>
